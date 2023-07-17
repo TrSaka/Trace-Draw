90ebd495274257bc7294bf1d341f10a5d6205f0c
Enes <86451907+TrSaka@users.noreply.github.com>
2023-07-17 17:29:49 +0300

    Update app_constants.dart

--- lib/core/constants/app/app_constants.dart
+++ lib/core/constants/app/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static String originalAdMobID = 'ca-app-pub-2389435520223654/5221155360';
-  static String defaultAdMobID = 'ca-app-pub-3940256099942544/6300978111';
+  static String defaultAdMobID = 'YOUR ADMOB ID';
 
   static String firebaseApplication = 'Application';
   static String firebaseVersion = 'versions';
